ea7a15e6b923ddc2388d232879f7c7b723b7c9b5
rifatbond1 <abdullah.al.rifat2239@gmail.com>
2026-02-25 22:37:58 +0600

    first commit

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import path from "path";
 
-dotenv.config({ path: path.join(process.cwd(), ".env") });
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 const config = {
   connection_str: process.env.CONNECTION_STR,
